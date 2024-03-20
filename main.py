79bbbadc095d6deff0825d6f10ca4ea20eac7cb9
Taxoz <admin@taxoz.dk>
2024-03-20 09:49:20 +0100

    added backup file being sent to remote FTP server

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 from datetime import date
 import os
 import zipfile
+import ftplib
 
 today = date.today()
 path = r"C:\dev\test"
+def ftpsendfil():
+    session = ftplib.FTP('IP','USERNAME','PASSWORD')
+    file = open(f'{today}.zip','rb')                  # file to send
+    session.storbinary(f'STOR {today}.zip', file)     # send the file
+    file.close()                                    # close file and FTP
+    session.quit()
 
 def zipdir(path, ziph):
     # ziph is zipfile handle
@@ -17,13 +24,15 @@ def zipdir(path, ziph):
 
 
 checkcheck = f"{today}.zip"
-
-
 isExist = os.path.exists(checkcheck) 
-print(isExist)
 
 if isExist == True:
     print(f"There is already a backup from {today}")
 else:
+    print("Making backup, please wait")
     with zipfile.ZipFile(f'{today}.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
         zipdir(f'{path}', zipf)
+        print("backup complate")
+        print("Senting files to remote server, pleasw wait")
+        ftpsendfil()
+        print("Update complate")
