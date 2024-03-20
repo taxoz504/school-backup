54898296394a795071a6c88abc85d6145e6a7231
Taxoz <admin@taxoz.dk>
2024-03-20 13:04:34 +0100

    main.py can now list items in the FTP server, server path setting moved to settings file

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 This script will take the folder in the path variable and zip it, then set it off to a remote server
 
 The settings.ini file needs to be edited for this to work with a vaild FTP server login and a path to the folder you want to backup
+for the SERVERPATH i.g path/to/folder - NOT path/to/folder/
 
--- main.py
+++ main.py
@@ -5,20 +5,47 @@ import ftplib
 import configparser
 
 today = date.today()
-def ftpsendfil():   
-    
+def ftpsendfil():
+    today = date.today()
+
     config = configparser.ConfigParser()
     config.read('settings.ini')
 
     ftp_ip = config['FTP']['IP']
     ftp_username = config['FTP']['USERNAME']
     ftp_password = config['FTP']['PASSWORD']
+    ftp_serverpath = config['FTP']['SERVERPATH']
 
     session = ftplib.FTP(ftp_ip, ftp_username, ftp_password)
+
+    # List files in the FTP server directory
+    files = session.nlst(ftp_serverpath)
+    print("List of files on FTP server:")
+    for file in files:
+        print(file)
+
     file = open(f'{today}.zip','rb')                  # file to send
-    session.storbinary(f'STOR {today}.zip', file)     # send the file
+    session.storbinary(f'STOR {ftp_serverpath}/{today}.zip', file)     # send the file
     file.close()                                    # close file and FTP
     session.quit()
+
+def listserverfiles():
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+
+    ftp_ip = config['FTP']['IP']
+    ftp_username = config['FTP']['USERNAME']
+    ftp_password = config['FTP']['PASSWORD']
+    ftp_serverpath = config['FTP']['SERVERPATH']
+
+    session = ftplib.FTP(ftp_ip, ftp_username, ftp_password)
+
+    # List files in the FTP server directory
+    files = session.nlst(ftp_serverpath)
+    print("List of files on FTP server:")
+    for file in files:
+        print(file)
+
 
 
     # zips a folder
@@ -42,7 +69,7 @@ checkcheck = f"{today}.zip"
 isExist = os.path.exists(checkcheck) 
 
     #   checks if a file with the same date as you run this script exist if 
-    #   not zips the path given in the top of the scirpt and sends it to FTP server delared in ftpsendfil
+    #   not zips the path given in the top of the scirpt and sends it to FTP server delared in settings.ini
 if isExist == True:
     print(f"There is already a backup from {today}")
 else:
